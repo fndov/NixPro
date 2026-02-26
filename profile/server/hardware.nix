70990736ba2abb6e65c9233d938131565a000494
miyu <tommybice1@gmail.com>
2026-02-25 18:29:34 -0600

    Wednesday Updates

--- profile/server/hardware.nix
+++ profile/server/hardware.nix
@@ -1,1 +1,6 @@
 { ... }: { }
+# Copy your hardwares configuration here, overwrite this empty file.
+# cp /etc/nixos/hardware-configuration.nix ~/.nixpro/profile/workstation/hardware.nix
+# If you don't have a hardware file: generate one, sudo nixos-generate-config
+# Then copy it over.
+# cp /etc/nixos/hardware-configuration.nix ~/.nixpro/profile/workstation/hardware.nix
